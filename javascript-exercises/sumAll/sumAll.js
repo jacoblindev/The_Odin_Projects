99b36ba0eab294ca9b2d81c153c74e5ddc70705d
Jacob <jacoblindev@gmail.com>
2021-01-30 15:32:49 +0800

    SumAll Done

--- javascript-exercises/sumAll/sumAll.js
+++ javascript-exercises/sumAll/sumAll.js
@@ -1,5 +1,14 @@
 const sumAll = function() {
-
+    let arr = [];
+    for (let arg of arguments) arr.push(arg);
+    let notPositiveNumbers = arr.map(e => typeof e !== 'number' ? 'ERROR' : e < 0 ? 'ERROR' : 0).includes('ERROR');
+    if (!notPositiveNumbers) {
+        let rs = 0;
+        for (let i = Math.min(...arr); i <= Math.max(...arr); i++) rs += i;
+        return rs;
+    } else {
+        return 'ERROR';
+    }
 }
 
 module.exports = sumAll
