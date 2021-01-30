90113d92d5d8ab9dfef69fac22a6a13e163c71f6
Jacob <jacoblindev@gmail.com>
2021-01-30 15:10:43 +0800

    RemoveFromArray Done

--- javascript-exercises/removeFromArray/removeFromArray.js
+++ javascript-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
 const removeFromArray = function() {
-
+    let rsArr = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        if (rsArr.includes(arguments[i])) rsArr.splice(rsArr.indexOf(arguments[i]), 1);
+    }
+    return rsArr;
 }
 
 module.exports = removeFromArray
