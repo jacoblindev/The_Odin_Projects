2c58935d04bf1017da13d24c069593bb2e9065ec
Jacob Lin <jacoblindev@gmail.com>
2021-06-02 22:08:11 +0800

    Try to see the err message

--- node_practices/demo_read_file.js
+++ node_practices/demo_read_file.js
@@ -3,6 +3,9 @@ let fs = require('fs');
 
 http.createServer((req, res) => {
     fs.readFile('demofile.html', (err, data) => {
+        if (err) {
+            console.error(err);
+        }
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
