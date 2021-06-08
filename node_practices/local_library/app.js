16cc46068de0a339d54070d03d96c3bcb0e66bd5
Jacob Lin <jacoblindev@gmail.com>
2021-06-08 22:32:46 +0800

    Setup mongoose & mongodb connection

--- node_practices/local_library/app.js
+++ node_practices/local_library/app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// mongoDB setup
+var mongoose = require('mongoose');
+var mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
